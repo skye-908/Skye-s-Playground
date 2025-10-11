0bbf84cb9eda6c92b67166add634aadf6f0a5402
skye-908 <rachel1512lam@gmail.com>
2025-10-11 23:50:07 +0100

    github link on nav bar deleted

--- src/config.ts
+++ src/config.ts
@@ -40,16 +40,7 @@ export const siteConfig: SiteConfig = {
 };
 
 export const navBarConfig: NavBarConfig = {
-	links: [
-		LinkPreset.Home,
-		LinkPreset.Archive,
-		LinkPreset.About,
-		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
-	],
+	links: [LinkPreset.Home, LinkPreset.Archive, LinkPreset.About],
 };
 
 export const profileConfig: ProfileConfig = {
